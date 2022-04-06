2019b6a96af8858d7453269a726096a28dfc3995
Daniel Ayane <danielayane@dhcp-10-31-113-229.dyn.MIT.EDU>
2022-04-06 17:46:59 -0400

    working simple website

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from search_engine import search_engine
+import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    global city
+    # ip_addr = request.remote_addr
+    # print(f"User's IP {ip_addr}")
+    # url = 'http://freegeoip.net/json'
+    # r = requests.get(url)
+    # print(r)
+    # j = json.loads(r.text)
+    # city = j['city']
+    city = "Cambridge MA"
     return render_template('index.html')
+
+
+@app.route('/search',methods=['GET'])
+def search():
+    query = request.args.get('query')
+    search_results = search_engine.search_func(query=f"{query} {city}")
+    if not search_results:
+        return no_results_template(query)
+    return render_template('search_results.html', search_results=search_results, query=query)
+
+def no_results_template(query):
+    return render_template('simple_message.html', title='No results found',
+                           message='Your search - <b>' + query + '</b> - did not match any documents.'
+                                                                 '<br>Suggestions:<br><ul>'
+                                                                 '<li></li>'
+                                                                 '<li>Try different keywords.</li>'
+                                                                 '<li>Try more general keywords.</li>'
+                                                                 '<li>Try fewer keywords.</ul>')
---
+++ search_engine/search_engine.py
@@ -0,0 +1,11 @@
+
+from googlesearch import search
+
+def search_func(query,  advanced=True):
+    search_result_return = []
+    search_results = search(query, num_results=3, advanced=True)
+    for result in search_results:
+        search_result_return.append(result)
+    return search_result_return
+
+print(search_func("test"))
